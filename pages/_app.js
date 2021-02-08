f08f4211f0fbd9ee6d50b19def7197445026e315
Rodman Swanston <swanston.rod@gmail.com>
2021-02-08 10:09:48 -0600

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,28 @@
-import '@/styles/global.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { ThemeProvider } from 'next-themes'
 
 import MDXComponents from '@/components/MDXComponents'
 import { MDXProvider } from '@mdx-js/react'
+import { pageview } from '@/lib/analytics'
+
+import '@/styles/global.css'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'production') return
+
+    const handleRouteChange = (url) => {
+      pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from '@/lib/analytics'
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
-          ></script>
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `
+            }}
+          />
           <link
             rel="preload"
             href="/fonts/inter-var-latin.woff2"
