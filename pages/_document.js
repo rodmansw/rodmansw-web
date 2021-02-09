a2f543ba4111e0d997973ea96f8a0c528b53e98a
Rodman Swanston <swanston.rod@gmail.com>
2021-02-08 21:37:53 -0600

    Remove gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,40 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-import { GA_TRACKING_ID } from '@/lib/analytics'
-
-function GaScript() {
-  if (process.env.NODE_ENV !== 'production') return null
-
-  return (
-    <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <script
-        defer
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <script
-        defer
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-        }}
-      />
-    </>
-  )
-}
-
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <GaScript />
           <link
             rel="preload"
             href="/fonts/inter-var-latin.woff2"
